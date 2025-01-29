9b47085e53c5d9521fb8e3638a46ed0bb15e62c9
PuszkaJakub <puszka.kontakt@gmail.com>
2025-01-29 16:17:04 +0100

    maybe will work

--- src/app/map-screen/map-screen.component.ts
+++ src/app/map-screen/map-screen.component.ts
@@ -1,43 +1,61 @@
-import { Component, OnInit, Output, EventEmitter, Input, inject } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  Input,
+  inject,
+} from '@angular/core';
+import * as L from 'leaflet';
 
+class Pizza {
+  name: string;
+  number: number;
+  category: string;
+  price: number;
+
+  constructor(name: string, number: number, category: string, price: number) {
+    this.name = name;
+    this.number = number;
+    this.category = category;
+    this.price = price;
+  }
+}
 
 @Component({
   selector: 'app-map-screen',
   imports: [],
   templateUrl: './map-screen.component.html',
-  styleUrl: './map-screen.component.scss'
+  styleUrl: './map-screen.component.scss',
 })
-export class MapScreenComponent implements OnInit{
+export class MapScreenComponent implements OnInit {
   @Output() callForData = new EventEmitter();
-  constructor() { }
+  constructor() {}
   private map: any;
 
-  ngOnInit(){
+  ngOnInit() {
     this.callForData.emit();
-    import('leaflet').then(L => {
-      this.initMap(L,);
-    });  
+
+    this.initMap();
+
   }
-  private initMap(L: typeof import('leaflet'), ): void {
+  private initMap(): void {
     this.map = L.map('map', {
-      center: [51.2771824,22.509986818957067], // Set map center
-      zoom: 20 // Set zoom level
+      center: [51.2771824, 22.509986818957067], // Set map center
+      zoom: 20, // Set zoom level
     });
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
-    const marker = L.marker([51.2771824,22.509986818957067]).addTo(this.map);
+    const marker = L.marker([51.2771824, 22.509986818957067]).addTo(this.map);
     marker.bindPopup('Sprawdź adres!').openPopup();
 
     // L.marker([52.2771824,22.509986818957067]).addTo(this.map)
   }
-  
 
-  centerMap(coords: [number, number]){
-    this.map.setView(coords)
+  centerMap(coords: [number, number]) {
+    this.map.setView(coords);
   }
-
-
-
 }
